430fcdf8a80b77d8eab815e65b549f25d17c4a6a
Radoslav <todorovradoslav6@gmail.com>
2025-07-04 23:28:24 +0300

    init express and port

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import 'dotenv/config'
+
+const PORT = process.env.PORT || 3030;
+
+
+const app = express();
+
+
+
+
+
+
+app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`))
